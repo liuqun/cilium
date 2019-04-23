a45b93873d170c8e23081cfc8ace050376202fad
Ian Vernon <ian@cilium.io>
2019-04-23 12:55:50 -0700

    contrib: fix up check-fmt.sh
    * Set the pipefail option so that the script fails when a piped command fails.
    This fixes the issue where `gofmt` was not actually run in the
    `make jenkins-precheck` target, as the commad to get all files to run `gofmt`
    failed when `./daemon/bindata.go` was not found, but the script itself did not
    exit with a non-zero return code. Said file was not able to be found in said
    target because `make` had not been ran yet. As a result, the `find` command
    silently failed, which resulted in the contents of the `diff` variable being
    empty, which the script assumed meant that there were no violations of `gofmt`.
    * Use bash as the shell for the script to be able to use the pipefail option.
    
    With these changes, the `jenkins-precheck` target should correctly catch `gofmt`
    violations on Jenkins.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- contrib/scripts/check-fmt.sh
+++ contrib/scripts/check-fmt.sh
@@ -1,13 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
-diff="$(find . ! \( -path './contrib' -prune \) \
+set -o pipefail
+
+if find ./daemon/bindata.go ; then
+  diff="$(find . ! \( -path './contrib' -prune \) \
         ! \( -path './vendor' -prune \) \
         ! \( -path './.git' -prune \) \
         ! \( -path '*.validate.go' -prune \) \
         ! -samefile ./daemon/bindata.go \
         -type f -name '*.go' -print0 \
                 | xargs -0 gofmt -d -l -s )"
+else
+  diff="$(find . ! \( -path './contrib' -prune \) \
+        ! \( -path './vendor' -prune \) \
+        ! \( -path './.git' -prune \) \
+        ! \( -path '*.validate.go' -prune \) \
+        -type f -name '*.go' -print0 \
+                | xargs -0 gofmt -d -l -s )"
+fi
 
 if [ -n "$diff" ]; then
 	echo "Unformatted Go source code:"
